9b27eb65f5f07205e355b9e7efd115211fec4680
hassaana <hassaan@graphiant.com>
2023-04-19 08:32:01 -0400

    Revert "Revert "Data Collection and Analysis scripts""
    This reverts commit bb334acc7421274395a722fb969731f70a5b58d5.

--- DataCollect.py
+++ DataCollect.py
@@ -2,24 +2,24 @@ from datetime import date
 from pytrends.request import TrendReq
 import os
 import json
+import time
 from time import sleep
 from math import ceil
 from googlesearch import search
 import requests
 import csv
-import sys
+import re
 from bs4 import BeautifulSoup
 
 
+
 #Save the top N search URLs
-URL_COUNT = 5
+URL_COUNT = 4
 URL_FILE = "URLs-record.csv"
-WEBSITE = "USCIS"
-PUBLIC_FIGURE = " Serena Williams"
+WEBSITE = ['bcs', 'united_airlines', 'usps', 'spectrum']
+PUBLIC_FIGURE = "Barack Obama"
 URL_HEADER = ['Website', 'PublicFigure', 'Question', 'SearchEngine'] + list('URL ' + str(index + 1) for index in range(URL_COUNT))
 
-# subscription key
-#BING_SUBSCRIPTION_KEY = 'd0138d4b9bf842c09dcba8d0058e0b74'
 def readCSV(filename):
     with open(filename, newline='') as csvfile:
         questions = list(csv.reader(csvfile, delimiter=','))
@@ -32,56 +32,43 @@ def appendCsv(filename, headers, data):
         file.write(','.join(data) + '\n')
 
 #Search Google Results
-def searchGoogleResults(query, Source, question):
-    results = list(search(query, num_results=URL_COUNT))
+def searchGoogleResults(query, Source, question, website):
+    results = list(search(query, num_results=URL_COUNT, lang="en"))
     print("Search Results for", query, ": ", results, "\n")
-    appendCsv(URL_FILE, URL_HEADER, [WEBSITE, PUBLIC_FIGURE, question, Source] + results)
+    appendCsv(URL_FILE, URL_HEADER, [website, PUBLIC_FIGURE, question, Source] + results)
     return results
 
 
+
+
 #webscrapping
-def webscrapper(URL, filename=PUBLIC_FIGURE):
+def webscrapper(URL, filename):
     #Make a request to URL
-    page = requests.get(URL)
+    params = {'q': 'python', 'key': 'AIzaSyCRBkVM2PRMGABvM2JARQRYcFOy9v4fmlg'}
+    page = requests.get(URL, params)
 
     # Parse the HTML content of the page
     soup = BeautifulSoup(page.content, "html.parser")
-    with open(filename, mode = 'a') as file:
+    all_text = soup.get_text(strip=True)
+    clean_text = re.sub(r"[^a-zA-Z0-9.]+", " ", all_text)
+    with open(filename, mode = 'a', buffering=1024) as file:
         if os.path.getsize(filename) == 0:
-            file.write(soup.get_text() + "\n")
-        file.write(soup.get_text() + "\n")
-
-
-
-
-'''
-#Search Bing Results
-def searchBingResults(bing_subscription_key,trendSource, trend):
-    search_url = "https://api.bing.microsoft.com/v7.0/search"
-    headers = {"Ocp-Apim-Subscription-Key": bing_subscription_key}
-    params = {
-        "q": trend,
-        "count": URL_COUNT,
-        "responseFilter": "Webpages",
-        "textDecorations": True,
-        "textFormat": "HTML"}
-    results = requests.get(search_url, headers = headers, params = params)
-    urllist = []
-    for i in range(0,10):
-        urllist.append(results.json()['webPages']['value'][i]['url'])
-    appendCsv(URL_FILE, URL_HEADER, [DATE, trendSource,'Bing', trend] + urllist)
-    
-'''        
+            file.write(clean_text + "\n")
+        file.write(clean_text + "\n")    
 
 
 if __name__ == "__main__":
-    with open("questions.txt" , "r") as f:
-        questions = [line.strip() for line in f]
+    for website in WEBSITE:
+        with open("questions" + '-' + website + '.txt' , "r") as f:
+            questions = [line.strip() for line in f]
 
 
-    for question in questions:
-        URLs = searchGoogleResults(question + PUBLIC_FIGURE, Source="Google", question=question)
-        for URL in URLs:
-            webscrapper(URL)
+        for question in questions:
+            URLs = searchGoogleResults(question + ' ' + PUBLIC_FIGURE, Source="Google", question=question, website=website)
+
+            for URL in URLs:
+                webscrapper(URL, PUBLIC_FIGURE + '_' + website + '.txt')
+                sleep(2)
+            sleep(5)
 
         
